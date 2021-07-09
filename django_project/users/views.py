469b6026c84cb0d7da5147ade74559cfff133242
Harshit Agarwal <harshit.agarwal212000@gmail.com>
2021-07-09 17:27:45 +0530

    changed register society page

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render,redirect
-
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 
@@ -7,16 +6,7 @@ from django.contrib import messages
 # Create your views here.
 
 def register(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
-    else:
-        form = UserCreationForm()
-    return render(request,'users/register.html',{'form': form})
+    return render(request,'users/register.html', {'title': 'register'})
 
 
 # messages.debug
