50943a2c857e50747f962982d2a5458d7ae9910e
Sparsh Agarwal <sagarwal_be19@thapar.edu>
2021-07-10 18:23:17 +0530

    Added Contact Us page

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home, name = 'blog-home'),
-    path('about/', views.about, name = 'blog-about'),
+    path('contact_us/', views.contact_us, name = 'blog-contact_us'),
 ]
