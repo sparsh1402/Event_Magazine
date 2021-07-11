53f56d1a49a2d3434a121a755d0357c94d67b4dc
Sparsh Agarwal <sagarwal_be19@thapar.edu>
2021-07-11 19:07:08 +0530

    Update_Profile

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 
 class Profile(models.Model):
@@ -8,5 +9,20 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 400 or img.weight > 400:
+            output_size = (400,400)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
+
+        
+
+
+
 
 # Create your models here.
