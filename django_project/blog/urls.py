8799475ada6c916bf0b05923c65fc4daf9d83747
Sparsh Agarwal <sagarwal_be19@thapar.edu>
2021-07-11 21:53:23 +0530

    Test

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name = 'blog-home'),
     path('contact_us/', views.contact_us, name = 'blog-contact_us'),
+    path('about/', views.about, name = 'blog-about'),
 ]
