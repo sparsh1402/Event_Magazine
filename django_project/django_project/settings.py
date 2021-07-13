d5b064dcbe749ae524cec5016a1bd0abc05f397b
Sparsh Agarwal <56266450+sparsh1402@users.noreply.github.com>
2021-07-14 02:11:02 +0530

    Update settings.py

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -1,5 +1,5 @@
 import os
-from settings.base import *
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
