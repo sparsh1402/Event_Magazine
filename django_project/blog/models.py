38c78ee22292cf55b930ca81d4a17aa5a071b448
Sparsh Agarwal <sagarwal_be19@thapar.edu>
2021-07-14 00:14:21 +0530

    Base Ready

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    date_posted = models.DateTimeField(default=timezone.now())  # User can change the time
+    date_posted = models.DateTimeField(default=timezone.now)  # User can change the time
     author = models.ForeignKey(User, on_delete=models.CASCADE) #if user is deleted then the post are also deleted 
     def __str__(self):
         return self.title
-# Create your models here.
+    
+    def get_absolute_url(self):
+        return reverse('post-detail',kwargs={'pk' : self.pk})
